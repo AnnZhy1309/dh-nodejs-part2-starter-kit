d7ad957560464bcb3074c57f03997aab8b2d3231
Anna Zhykol <annzhy1309@gmail.com>
2023-07-11 20:03:06 +0300

    2 ex 2

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,22 @@
-const TODO: string = "start writing your Express API server here :)";
+import express from "express";
+import {Request, Response } from "express";
+import 'express-async-errors'
+import morgan from 'morgan';
+import { getAll, getOneById, create, updateById, deleteById } from "./controller/planets";
+import { unzipSync } from "zlib";
 
-console.log(TODO);
+const app = express();
+const port = 3000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get('/api/planets', getAll);
+app.get('/api/planets', getOneById);
+app.post('/api/planets', create);
+app.put('/api/planets', updateById);
+app.delete('/api/planets', deleteById);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
