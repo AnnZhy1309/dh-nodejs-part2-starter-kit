634184c3f73e6f0d355dad16bbab6e0af3833bdd
Anna Zhykol <annzhy1309@gmail.com>
2023-07-11 20:01:47 +0300

    ex 2

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,30 @@
-const TODO: string = "start writing your Express API server here :)";
+import express from "express";
+import {Request, Response } from "express";
+import 'express-async-errors'
+import morgan from 'morgan';
 
-console.log(TODO);
+const app = express();
+const port = 3000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+type Planet = {
+    id: number,
+    name: string,
+};
+
+type Planets = Planet[];
+
+let planets = [
+    {id: 1, name: "Earth"},
+    {id:2, name: "Mars"}
+];
+
+app.get('/api/planets', (req: Request, res: Response) => {
+  res.status(200).json(planets)
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
